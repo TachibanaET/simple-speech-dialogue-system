00fe71f7719df2643aefceb01e10895898492267
Enfu.G <guoenfu321@gmail.com>
2021-04-13 15:01:26 +0900

    change vad

--- client/webserver/websource/js/vad.js
+++ client/webserver/websource/js/vad.js
@@ -1,227 +1,129 @@
-// source : https://github.com/kdavis-mozilla/vad.js
-(function (window) {
+// source : https://github.com/Jam3/voice-activity-detection/blob/master/index.js
+'use strict';
+var analyserFrequency = require('analyser-frequency-average');
 
-  var VAD = function (options) {
-    // Default options
-    this.options = {
-      fftSize: 512,
-      bufferLen: 512,
-      voice_stop: function () { },
-      voice_start: function () { },
-      smoothingTimeConstant: 0.99,
-      energy_offset: 1e-8, // The initial offset.
-      energy_threshold_ratio_pos: 2, // Signal must be twice the offset
-      energy_threshold_ratio_neg: 0.5, // Signal must be half the offset
-      energy_integration: 1, // Size of integration change compared to the signal per second.
-      filter: [
-        { f: 200, v: 0 }, // 0 -> 200 is 0
-        { f: 2000, v: 1 } // 200 -> 2k is 1
-      ],
-      source: null,
-      context: null
-    };
+module.exports = function (audioContext, stream, opts) {
 
-    // User options
-    for (var option in options) {
-      if (options.hasOwnProperty(option)) {
-        this.options[option] = options[option];
-      }
-    }
+  opts = opts || {};
 
-    // Require source
-    if (!this.options.source)
-      throw new Error("The options must specify a MediaStreamAudioSourceNode.");
-
-    // Set this.options.context
-    this.options.context = this.options.source.context;
-
-    // Calculate time relationships
-    this.hertzPerBin = this.options.context.sampleRate / this.options.fftSize;
-    this.iterationFrequency = this.options.context.sampleRate / this.options.bufferLen;
-    this.iterationPeriod = 1 / this.iterationFrequency;
-
-    var DEBUG = true;
-    if (DEBUG) console.log(
-      'Vad' +
-      ' | sampleRate: ' + this.options.context.sampleRate +
-      ' | hertzPerBin: ' + this.hertzPerBin +
-      ' | iterationFrequency: ' + this.iterationFrequency +
-      ' | iterationPeriod: ' + this.iterationPeriod
-    );
-
-    this.setFilter = function (shape) {
-      this.filter = [];
-      for (var i = 0, iLen = this.options.fftSize / 2; i < iLen; i++) {
-        this.filter[i] = 0;
-        for (var j = 0, jLen = shape.length; j < jLen; j++) {
-          if (i * this.hertzPerBin < shape[j].f) {
-            this.filter[i] = shape[j].v;
-            break; // Exit j loop
-          }
-        }
-      }
-    }
-
-    this.setFilter(this.options.filter);
-
-    this.ready = {};
-    this.vadState = false; // True when Voice Activity Detected
-
-    // Energy detector props
-    this.energy_offset = this.options.energy_offset;
-    this.energy_threshold_pos = this.energy_offset * this.options.energy_threshold_ratio_pos;
-    this.energy_threshold_neg = this.energy_offset * this.options.energy_threshold_ratio_neg;
-
-    this.voiceTrend = 0;
-    this.voiceTrendMax = 10;
-    this.voiceTrendMin = -10;
-    this.voiceTrendStart = 5;
-    this.voiceTrendEnd = -5;
-
-    // Create analyser 
-    this.analyser = this.options.context.createAnalyser();
-    this.analyser.smoothingTimeConstant = this.options.smoothingTimeConstant; // 0.99;
-    this.analyser.fftSize = this.options.fftSize;
-
-    this.floatFrequencyData = new Float32Array(this.analyser.frequencyBinCount);
-
-    // Setup local storage of the Linear FFT data
-    this.floatFrequencyDataLinear = new Float32Array(this.floatFrequencyData.length);
-
-    // Connect this.analyser
-    this.options.source.connect(this.analyser);
-
-    // Create ScriptProcessorNode
-    this.scriptProcessorNode = this.options.context.createScriptProcessor(this.options.bufferLen, 1, 1);
-
-    // Connect scriptProcessorNode (Theretically, not required)
-    this.scriptProcessorNode.connect(this.options.context.destination);
-
-    // Create callback to update/analyze floatFrequencyData
-    var self = this;
-    this.scriptProcessorNode.onaudioprocess = function (event) {
-      self.analyser.getFloatFrequencyData(self.floatFrequencyData);
-      self.update();
-      self.monitor();
-    };
-
-    // Connect scriptProcessorNode
-    this.options.source.connect(this.scriptProcessorNode);
-
-    // log stuff
-    this.logging = false;
-    this.log_i = 0;
-    this.log_limit = 100;
-
-    this.triggerLog = function (limit) {
-      this.logging = true;
-      this.log_i = 0;
-      this.log_limit = typeof limit === 'number' ? limit : this.log_limit;
-    }
-
-    this.log = function (msg) {
-      if (this.logging && this.log_i < this.log_limit) {
-        this.log_i++;
-        console.log(msg);
-      } else {
-        this.logging = false;
-      }
-    }
-
-    this.update = function () {
-      // Update the local version of the Linear FFT
-      var fft = this.floatFrequencyData;
-      for (var i = 0, iLen = fft.length; i < iLen; i++) {
-        this.floatFrequencyDataLinear[i] = Math.pow(10, fft[i] / 10);
-      }
-      this.ready = {};
-    }
-
-    this.getEnergy = function () {
-      if (this.ready.energy) {
-        return this.energy;
-      }
-
-      var energy = 0;
-      var fft = this.floatFrequencyDataLinear;
-
-      for (var i = 0, iLen = fft.length; i < iLen; i++) {
-        energy += this.filter[i] * fft[i] * fft[i];
-      }
-
-      this.energy = energy;
-      this.ready.energy = true;
-
-      return energy;
-    }
-
-    this.monitor = function () {
-      var energy = this.getEnergy();
-      var signal = energy - this.energy_offset;
-
-      if (signal > this.energy_threshold_pos) {
-        this.voiceTrend = (this.voiceTrend + 1 > this.voiceTrendMax) ? this.voiceTrendMax : this.voiceTrend + 1;
-      } else if (signal < -this.energy_threshold_neg) {
-        this.voiceTrend = (this.voiceTrend - 1 < this.voiceTrendMin) ? this.voiceTrendMin : this.voiceTrend - 1;
-      } else {
-        // voiceTrend gets smaller
-        if (this.voiceTrend > 0) {
-          this.voiceTrend--;
-        } else if (this.voiceTrend < 0) {
-          this.voiceTrend++;
-        }
-      }
-
-      var start = false, end = false;
-      if (this.voiceTrend > this.voiceTrendStart) {
-        // Start of speech detected
-        start = true;
-      } else if (this.voiceTrend < this.voiceTrendEnd) {
-        // End of speech detected
-        end = true;
-      }
-
-      // Integration brings in the real-time aspect through the relationship with the frequency this functions is called.
-      var integration = signal * this.iterationPeriod * this.options.energy_integration;
-
-      // Idea?: The integration is affected by the voiceTrend magnitude? - Not sure. Not doing atm.
-
-      // The !end limits the offset delta boost till after the end is detected.
-      if (integration > 0 || !end) {
-        this.energy_offset += integration;
-      } else {
-        this.energy_offset += integration * 10;
-      }
-      this.energy_offset = this.energy_offset < 0 ? 0 : this.energy_offset;
-      this.energy_threshold_pos = this.energy_offset * this.options.energy_threshold_ratio_pos;
-      this.energy_threshold_neg = this.energy_offset * this.options.energy_threshold_ratio_neg;
-
-      // Broadcast the messages
-      if (start && !this.vadState) {
-        this.vadState = true;
-        this.options.voice_start();
-      }
-      if (end && this.vadState) {
-        this.vadState = false;
-        this.options.voice_stop();
-      }
-
-      this.log(
-        'e: ' + energy +
-        ' | e_of: ' + this.energy_offset +
-        ' | e+_th: ' + this.energy_threshold_pos +
-        ' | e-_th: ' + this.energy_threshold_neg +
-        ' | signal: ' + signal +
-        ' | int: ' + integration +
-        ' | voiceTrend: ' + this.voiceTrend +
-        ' | start: ' + start +
-        ' | end: ' + end
-      );
-
-      return signal;
+  var defaults = {
+    fftSize: 1024,
+    bufferLen: 1024,
+    smoothingTimeConstant: 0.2,
+    minCaptureFreq: 85,         // in Hz
+    maxCaptureFreq: 255,        // in Hz
+    noiseCaptureDuration: 1000, // in ms
+    minNoiseLevel: 0.3,         // from 0 to 1
+    maxNoiseLevel: 0.7,         // from 0 to 1
+    avgNoiseMultiplier: 1.2,
+    onVoiceStart: function () {
+    },
+    onVoiceStop: function () {
+    },
+    onUpdate: function (val) {
     }
   };
 
-  window.VAD = VAD;
+  var options = {};
+  for (var key in defaults) {
+    options[key] = opts.hasOwnProperty(key) ? opts[key] : defaults[key];
+  }
 
-})(window);
+  var baseLevel = 0;
+  var voiceScale = 1;
+  var activityCounter = 0;
+  var activityCounterMin = 0;
+  var activityCounterMax = 60;
+  var activityCounterThresh = 5;
+
+  var envFreqRange = [];
+  var isNoiseCapturing = true;
+  var prevVadState = undefined;
+  var vadState = false;
+  var captureTimeout = null;
+
+  var source = audioContext.createMediaStreamSource(stream);
+  var analyser = audioContext.createAnalyser();
+  analyser.smoothingTimeConstant = options.smoothingTimeConstant;
+  analyser.fftSize = options.fftSize;
+
+  var scriptProcessorNode = audioContext.createScriptProcessor(options.bufferLen, 1, 1);
+  connect();
+  scriptProcessorNode.onaudioprocess = monitor;
+
+  if (isNoiseCapturing) {
+    //console.log('VAD: start noise capturing');
+    captureTimeout = setTimeout(init, options.noiseCaptureDuration);
+  }
+
+  function init() {
+    //console.log('VAD: stop noise capturing');
+    isNoiseCapturing = false;
+
+    envFreqRange = envFreqRange.filter(function (val) {
+      return val;
+    }).sort();
+    var averageEnvFreq = envFreqRange.length ? envFreqRange.reduce(function (p, c) { return Math.min(p, c) }, 1) : (options.minNoiseLevel || 0.1);
+
+    baseLevel = averageEnvFreq * options.avgNoiseMultiplier;
+    if (options.minNoiseLevel && baseLevel < options.minNoiseLevel) baseLevel = options.minNoiseLevel;
+    if (options.maxNoiseLevel && baseLevel > options.maxNoiseLevel) baseLevel = options.maxNoiseLevel;
+
+    voiceScale = 1 - baseLevel;
+
+    //console.log('VAD: base level:', baseLevel);
+  }
+
+  function connect() {
+    source.connect(analyser);
+    analyser.connect(scriptProcessorNode);
+    scriptProcessorNode.connect(audioContext.destination);
+  }
+
+  function disconnect() {
+    scriptProcessorNode.disconnect();
+    analyser.disconnect();
+    source.disconnect();
+  }
+
+  function destroy() {
+    captureTimeout && clearTimeout(captureTimeout);
+    disconnect();
+    scriptProcessorNode.onaudioprocess = null;
+  }
+
+  function monitor() {
+    var frequencies = new Uint8Array(analyser.frequencyBinCount);
+    analyser.getByteFrequencyData(frequencies);
+
+    var average = analyserFrequency(analyser, frequencies, options.minCaptureFreq, options.maxCaptureFreq);
+    if (isNoiseCapturing) {
+      envFreqRange.push(average);
+      return;
+    }
+
+    if (average >= baseLevel && activityCounter < activityCounterMax) {
+      activityCounter++;
+    } else if (average < baseLevel && activityCounter > activityCounterMin) {
+      activityCounter--;
+    }
+    vadState = activityCounter > activityCounterThresh;
+
+    if (prevVadState !== vadState) {
+      vadState ? onVoiceStart() : onVoiceStop();
+      prevVadState = vadState;
+    }
+
+    options.onUpdate(Math.max(0, average - baseLevel) / voiceScale);
+  }
+
+  function onVoiceStart() {
+    options.onVoiceStart();
+  }
+
+  function onVoiceStop() {
+    options.onVoiceStop();
+  }
+
+  return { connect: connect, disconnect: disconnect, destroy: destroy };
+};
